6eddcab4ed99c6ee06547b970868b83e16ebbccd
mserhii1029 <mserhii1029@gmail.com>
2022-04-23 06:29:56 +0300

    Update ChatInput.js

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,13 +1,13 @@
-import { Button } from "@material-ui/core";
-import React from "react";
-import styled from "styled-components";
-import { useState } from "react";
-import { db, auth } from "../Firebase";
-import firebase from "firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { Button } from '@material-ui/core';
+import React from 'react';
+import styled from 'styled-components';
+import { useState } from 'react';
+import { db, auth } from '../Firebase';
+import firebase from 'firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 function ChatInput({ channelId, channelName, chatRef }) {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
   const [user] = useAuthState(auth);
 
   const sendMessage = (e) => {
@@ -15,29 +15,25 @@ function ChatInput({ channelId, channelName, chatRef }) {
     if (!channelId) {
       return false;
     }
-    db.collection("rooms").doc(channelId).collection("messages").add({
+    db.collection('rooms').doc(channelId).collection('messages').add({
       message: input,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      user: "Udit Takkar",
+      user: 'Serhii Muzyka',
       userImage: user.photoURL,
     });
 
     chatRef.current.scrollIntoView({
-      behavior: "smooth",
+      behavior: 'smooth',
     });
 
-    setInput("");
+    setInput('');
   };
 
   return (
     <ChatInputContainer>
       <form>
-        <input
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder={`Message #${channelName}`}
-        />
-        <Button onClick={sendMessage} hidden type="submit">
+        <input value={input} onChange={(e) => setInput(e.target.value)} placeholder={`Message #${channelName}`} />
+        <Button onClick={sendMessage} hidden type='submit'>
           SEND
         </Button>
       </form>
